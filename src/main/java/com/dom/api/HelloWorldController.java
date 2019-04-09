89728f4626208f18015bad5da786bba1e4c3923b
Diogo Monte <dom@Diogos-MacBook-Pro.local>
2019-04-09 16:54:45 +0200

    example with environment variables

--- src/main/java/com/dom/api/HelloWorldController.java
+++ src/main/java/com/dom/api/HelloWorldController.java
@@ -1,11 +1,27 @@
 package com.dom.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 @RestController
 public class HelloWorldController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+
+	@Value("${service.url}")
+	private String serviceUrl;
+	@Value("${service.port}")
+	private String servicePort;
 
 	@GetMapping("/")
 	public ResponseEntity works() {
@@ -17,5 +33,24 @@ public class HelloWorldController {
 		return ResponseEntity.ok("Hello world!");
 	}
 
+	@GetMapping("/health")
+	public ResponseEntity health() throws IOException {
+		logger.info("Service url: " + serviceUrl + " - port: " + servicePort);
+
+		URL url = new URL("http://" + serviceUrl + ":" + servicePort + "/");
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		String inputLine;
+		StringBuffer content = new StringBuffer();
+		while ((inputLine = in.readLine()) != null) {
+			content.append(inputLine);
+		}
+		in.close();
+		connection.disconnect();
+
+		return ResponseEntity.status(connection.getResponseCode()).body(content.toString());
+	}
 
 }
--- src/test/java/com/dom/testcontainers/GenericContainerTest.java
+++ src/test/java/com/dom/testcontainers/GenericContainerTest.java
@@ -8,16 +8,18 @@ import org.junit.Test;
 import org.testcontainers.containers.GenericContainer;
 
 import java.time.Duration;
+import java.util.Map;
 
 public class GenericContainerTest {
 
 	@Rule
 	public GenericContainer container = new GenericContainer("diogomonte/rest-api-application")
 			.withStartupTimeout(Duration.ofSeconds(30))
-			.withExposedPorts(8080);
+			.withExposedPorts(8080)
+			.withEnv(Map.of("SERVICE_URL","localhost", "SERVICE_PORT","8080"));
 
 	@Test
-	public void itShouldTestHelloWorldContainer() {
+	public void itShouldTestHelloRequest() {
 		RestAssured.baseURI = "http://localhost";
 		RestAssured.port = container.getMappedPort(8080);
 
@@ -25,6 +27,17 @@ public class GenericContainerTest {
 				.when().get("/hello")
 				.then().assertThat().statusCode(200)
 				.and().body(CoreMatchers.equalTo("Hello world!"));
+	}
+
+	@Test
+	public void itShouldTestHealthRequest() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = container.getMappedPort(8080);
+
+		RestAssured.given().accept(ContentType.JSON)
+				.when().get("/health")
+				.then().assertThat().statusCode(200)
+				.and().body(CoreMatchers.equalTo("It works!"));
 	}
 
 }
